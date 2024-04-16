54de7634c0f4e6af24d65d8457b90a4ab9d2e42a
rhkdgur <rhkdgur195@gmail.com>
2024-04-17 01:05:04 +0900

    첫 환경세팅 다시

---
+++ src/main/java/com/flab/blackfriday/BlackfridayApplication.java
@@ -0,0 +1,13 @@
+package com.flab.blackfriday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackfridayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackfridayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/blackfriday/BlackfridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.blackfriday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackfridayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
